3bf1324c135385692c8e6202adf79d067e617a64
unknown <malisheregaming@gmail.com>
2026-04-29 13:20:14 +0800

    add tutorial 3 question 4

---
+++ T3Q4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void displayVector(float *a_array, int numElem);
+void getInputIntoVector(float *a_array, int numElem);
+
+int main() {
+    int numElem;
+
+    cout << "Enter how many numbers to enter >> ";
+    cin >> numElem;
+
+    float *arr = new float[numElem];
+
+    getInputIntoVector(arr, numElem);
+    displayVector(arr, numElem);
+
+    delete[] arr;
+
+    return 0;
+}
+
+void getInputIntoVector(float *a_array, int numElem) {
+    cout << "Enter " << numElem << " floating point numbers:\n";
+    for (int i = 0; i < numElem; i++) {
+        cin >> a_array[i];
+    }
+}
+
+void displayVector(float *a_array, int numElem) {
+    cout << "Display data in array \n";
+    for (int i = 0; i < numElem; i++) {
+        cout << a_array[i] << " ";
+    }
+    cout << endl;
+}
